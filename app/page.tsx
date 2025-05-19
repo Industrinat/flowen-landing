7c6abdb7b423c520a435af7d1816b666e116f450
Daniel OIsson <daniel.olsson@industrinat.se>
2025-05-19 21:26:39 +0200

    Lägg till fungerande startsida

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main style={{ height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      <h1 style={{ fontSize: '3rem' }}>Hej från Flowen 🚀</h1>
+    </main>
+  );
+}
