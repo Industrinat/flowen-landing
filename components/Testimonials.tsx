341adc3f59b8ec2ae12d6facfb5acc3723cd0f30
Daniel OIsson <daniel.olsson@industrinat.se>
2025-05-29 23:16:35 +0200

    Fixade Testimonials-komponenten och carousel bug

--- components/Testimonials.tsx
+++ components/Testimonials.tsx
@@ -1,10 +1,11 @@
 'use client'
 
-
+import React from "react"; // ← denna behövs för att ESLint ska vara nöjd
 import { useState, useEffect } from 'react'
 import { Card, CardContent, CardFooter, CardHeader } from '@/components/ui/card'
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import { cn } from '@/lib/utils'
+
 
 type Testimonial = {
   id: number
